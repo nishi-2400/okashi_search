7d872b089a9cb1ebe752082424642f35bc7db8ce
nishi-2400 <a7w21087bump@yahoo.co.jp>
2019-12-04 19:42:48 +0900

    Initial Commit

---
+++ MyOkashi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyOkashi
+//
+//  Created by Yusuke Nishi on 2019/12/04.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
